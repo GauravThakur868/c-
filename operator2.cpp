716861120c36987552ea2111d78989e567700682
GAURAV <gt267200@gmail.com>
2023-10-10 21:53:10 +0530

    1

---
+++ operator2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class Number {
+	private:
+		int a,b,c;
+	public:
+		Number(int x,int y,int z){
+			a=x;
+			b=y;
+			c=z;
+		}
+		void operator++(){
+			a=++a;
+			b=++b;
+			c=++c;
+		}
+		void operator--(){
+			a=--a;
+			b=--b;
+			c=--c;
+		}
+		void show(){
+			cout<<"a = "<<a<<endl;
+			cout<<"b = "<<b<<endl;
+			cout<<"c = "<<c<<endl;
+		}
+};
+int main(){
+	Number N(3,4,5);
+	Number M(3,4,5);
+	N.operator++();
+	N.show();
+	M.operator--();
+	M.show();
+}

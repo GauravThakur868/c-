2c1271dc7f455526684e90e5b2641af018c73a1e
GAURAV <gt267200@gmail.com>
2023-10-19 23:17:32 +0530

    1

---
+++ class2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class A{
+	protected:
+		int x;
+	public:
+		void input()  {
+			cout<<"\n enter x: ";
+			cin>>x;
+		}
+};
+class B:public A{
+	public:
+		void display() {
+			cout<<"square is: "<<x*x;
+		}
+};
+class C:public A{
+	public:
+		void display(){
+			int fact=1;
+			for(int i=1;i<=x;i++){
+				fact = fact*i;
+			}
+			cout<<"\n Factorial is: "<<fact;
+		}
+};
+int main(){
+	B obj1;
+	obj1.input();
+	obj1.display();
+	C obj2;
+	obj2.input();
+	obj2.display();
+	return 0;
+}

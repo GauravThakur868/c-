6b3d670945b8b6d4d2b0645ef7aaba862a36b285
GAURAV <gt267200@gmail.com>
2023-11-07 22:11:06 +0530

    1

---
+++ dma_in_class.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class Array{
+	int *arr;
+	int size;
+	public:
+		void get_data(int n){
+			size= n;
+			arr=new int[size];
+			cout<<"enter the values: ";
+			for(int i=0;i<size;i++){
+				cin>>arr[i];
+			}
+		}
+		int get_sum(){
+			int sum = 0;
+			for(int i=0;i<size;i++){
+				sum = sum+arr[i];
+			}
+			return sum;
+		}
+		void display_data(){
+			for(int i=0;i<size;i++){
+				cout<<"\t"<<arr[i];
+			}
+			cout<<"\n Sum of elements="<<get_sum();
+		}
+		~Array(){
+			delete[]arr;
+			cout<<"\n Memory deallocated";
+		}
+};
+int main(){
+	Array a;
+	int n;
+	cout<<"\n Enter the number of elements:"<<endl;
+	cin>>n;
+	a.get_data(n);
+	a.display_data();
+}

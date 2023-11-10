4906d3a507860890d1395c5360f417127fee8211
GAURAV <gt267200@gmail.com>
2023-11-10 23:55:48 +0530

    1

---
+++ polymorph.cpp
@@ -0,0 +1,31 @@
+
+#include<iostream>
+using namespace std;
+class area {
+	private:
+		int p;
+		int q;
+		float r;
+	public:
+		int area(int a){
+			p = a;
+			cout<<"area of square: ";
+		}
+		int area(int a,int b){
+			p=a;
+			q=b;
+			cout<<"area of rectangle: "<<;	
+		}	
+		float area(float a){
+			r = a;
+			cout<<"area of circle: "<<;
+		}
+};
+int main()
+{
+	int x,y,z;
+	float v;
+	cout<<"enter the value: "<<endl;
+	cin>>x>>y>>z>>v;
+	area()
+}
